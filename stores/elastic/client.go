7bdcb2a70ab841ba8ac8e4355b8913ef80b24500
#Suyghur <suyghurmjp@outlook.com>
2023-01-12 16:40:48 +0800

    feat: delete insert() and query() func of es pkg

--- stores/elastic/client.go
+++ stores/elastic/client.go
@@ -14,16 +14,14 @@ type (
 		password   string
 		enableAuth bool
 		enableGzip bool
-		instance   *elastic.Client
+		*elastic.Client
 	}
 
 	IEsClient interface {
 		Query(handle func(srv *elastic.SearchService) *elastic.SearchService) *elastic.SearchResult
 		QueryCtx(ctx context.Context, handle func(srv *elastic.SearchService) *elastic.SearchService) *elastic.SearchResult
-		query(ctx context.Context, handle func(srv *elastic.SearchService) *elastic.SearchService) *elastic.SearchResult
 		Insert(index string, data interface{})
 		InsertCtx(ctx context.Context, index string, data interface{})
-		insert(ctx context.Context, index string, data interface{})
 	}
 )
 
@@ -40,7 +38,7 @@ func New(endpoints []string, opts ...Option) IEsClient {
 		logx.WithContext(context.Background()).Error(err.Error())
 		panic(err.Error())
 	} else {
-		impl.instance = cli
+		impl.Client = cli
 	}
 	return impl
 }
@@ -66,14 +64,17 @@ func (impl *client) Query(handle func(srv *elastic.SearchService) *elastic.Searc
 func (impl *client) QueryCtx(ctx context.Context, handle func(srv *elastic.SearchService) *elastic.SearchService) *elastic.SearchResult {
 	var result *elastic.SearchResult
 	xtrace.StartFuncSpan(ctx, "QueryDataFromEs", func(ctx context.Context) {
-		result = impl.query(ctx, handle)
+		if ret, err := handle(impl.Search()).Pretty(true).Do(ctx); err != nil {
+			logx.WithContext(ctx).Errorf("query data on error: %v", err)
+		} else {
+			result = ret
+		}
 	})
 	return result
 }
 
 func (impl *client) query(ctx context.Context, handle func(srv *elastic.SearchService) *elastic.SearchService) *elastic.SearchResult {
-	impl.instance.IngestGetPipeline()
-	if result, err := handle(impl.instance.Search()).Pretty(true).Do(ctx); err != nil {
+	if result, err := handle(impl.Search()).Pretty(true).Do(ctx); err != nil {
 		logx.WithContext(ctx).Errorf("query data on error: %v", err)
 		return nil
 	} else {
@@ -87,14 +88,10 @@ func (impl *client) Insert(index string, data interface{}) {
 
 func (impl *client) InsertCtx(ctx context.Context, index string, data interface{}) {
 	xtrace.StartFuncSpan(ctx, "InsertDataToEs", func(ctx context.Context) {
-		impl.insert(ctx, index, data)
+		if result, err := impl.Index().Index(index).Refresh("false").BodyJson(data).Do(ctx); err != nil {
+			logx.WithContext(ctx).Errorf("insert data on error: %v", err)
+		} else {
+			logx.WithContext(ctx).Infof("insert data on success: %+v", *result)
+		}
 	})
-}
-
-func (impl *client) insert(ctx context.Context, index string, data interface{}) {
-	if result, err := impl.instance.Index().Index(index).Refresh("false").BodyJson(data).Do(ctx); err != nil {
-		logx.WithContext(ctx).Errorf("insert data on error: %v", err)
-	} else {
-		logx.WithContext(ctx).Infof("insert data on success: %+v", *result)
-	}
 }
