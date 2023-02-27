d78c0b140d03b7e1e36070950cfc9c3731ae33ad
zengkaichun <kaishinz@qq.com>
2023-02-27 16:39:03 +0800

    feat(xtrace): add method `TracingOnApiSvr`

--- xtrace/helper.go
+++ xtrace/helper.go
@@ -5,9 +5,13 @@
 package xtrace
 
 import (
+	"bytes"
 	"context"
+	"github.com/zeromicro/go-zero/rest"
 	"go.opentelemetry.io/otel/attribute"
 	"go.opentelemetry.io/otel/trace"
+	"io/ioutil"
+	"net/http"
 )
 
 func GetTraceId(ctx context.Context) trace.TraceID {
@@ -32,4 +36,31 @@ func AddEvent(ctx context.Context, name string, kv ...attribute.KeyValue) {
 	*/
 	span := trace.SpanFromContext(ctx)
 	span.AddEvent(name, trace.WithAttributes(kv...))
+}
+
+func TracingOnApiSvr(server *rest.Server) {
+	/*
+		running go-zero api server
+	*/
+	server.Use(func(next http.HandlerFunc) http.HandlerFunc {
+		return func(w http.ResponseWriter, r *http.Request) {
+
+			bytesData, err := ioutil.ReadAll(r.Body)
+			if err == nil {
+				r.Body = ioutil.NopCloser(bytes.NewBuffer(bytesData))
+				jsonString := string(bytesData)
+
+				// todo body parsed into tags
+				//var data map[string]interface{}
+				//err = json.Unmarshal([]byte(jsonString), &data)
+				//if err != nil {
+				//	next(w, r)
+				//}
+
+				// write body info into events
+				AddEvent(r.Context(), r.RequestURI, attribute.String("params", jsonString))
+			}
+			next(w, r)
+		}
+	})
 }
