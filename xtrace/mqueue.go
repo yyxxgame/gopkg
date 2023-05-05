9530d8f52bec2603da112fb41f2777bee14abcc1
lvwenqi <769972594@qq.com>
2023-05-05 14:27:12 +0800

    feat(xtrance): mqueue producer and consumer trace

---
+++ xtrace/mqueue.go
@@ -0,0 +1,39 @@
+//@File     mqueue.go
+//@Time     2023/05/04
+//@Author   LvWenQi
+
+package xtrace
+
+import (
+	"context"
+	"encoding/json"
+	gozerotrace "github.com/zeromicro/go-zero/core/trace"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/trace"
+	oteltrace "go.opentelemetry.io/otel/trace"
+)
+
+type HeaderCarrier struct {
+	Carrier *propagation.HeaderCarrier `json:"carrier"`
+}
+
+func StartMqProducerTrace(ctx context.Context, name string, kv ...attribute.KeyValue) (trace.Span, *HeaderCarrier) {
+	tracer := otel.GetTracerProvider().Tracer(gozerotrace.TraceName)
+	spanCtx, span := tracer.Start(ctx, name, oteltrace.WithSpanKind(oteltrace.SpanKindProducer))
+	carrier := &propagation.HeaderCarrier{}
+	otel.GetTextMapPropagator().Inject(spanCtx, carrier)
+	span.AddEvent(name, oteltrace.WithAttributes(kv...))
+	return span, &HeaderCarrier{Carrier: carrier}
+}
+
+func StartMqConsumerTrace(ctx context.Context, name string, msgStr string, kv ...attribute.KeyValue) trace.Span {
+	var carrier HeaderCarrier
+	_ = json.Unmarshal([]byte(msgStr), &carrier)
+	wireContext := otel.GetTextMapPropagator().Extract(ctx, carrier.Carrier)
+	tracer := otel.GetTracerProvider().Tracer(gozerotrace.TraceName)
+	_, span := tracer.Start(wireContext, name, oteltrace.WithSpanKind(oteltrace.SpanKindConsumer))
+	span.AddEvent(name, oteltrace.WithAttributes(kv...))
+	return span
+}
