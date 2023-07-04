3480bac3f3e7bf5bd52fe5901c1eccf77c42600e
#Suyghur <suyghurmjp@outlook.com>
2023-07-04 16:32:24 +0800

    feat(mq): added context in saramakafka consumer handler (#36)

--- mq/saramakafka/consumer.go
+++ mq/saramakafka/consumer.go
@@ -7,9 +7,11 @@ package saramakafka
 import (
 	"context"
 	"github.com/Shopify/sarama"
+	"github.com/yyxxgame/gopkg/mq"
 	"github.com/yyxxgame/gopkg/syncx/gopool"
 	"github.com/yyxxgame/gopkg/xtrace"
 	"github.com/zeromicro/go-zero/core/logx"
+	"go.opentelemetry.io/otel/attribute"
 	oteltrace "go.opentelemetry.io/otel/trace"
 	"sync"
 )
@@ -31,7 +33,7 @@ type (
 		*config
 	}
 
-	ConsumerHandler func(message *sarama.ConsumerMessage) error
+	ConsumerHandler func(ctx context.Context, message *sarama.ConsumerMessage) error
 )
 
 func NewSaramaConsumer(brokers, topics []string, groupId string, opts ...Option) IConsumer {
@@ -96,16 +98,19 @@ func (c *consumer) Cleanup(session sarama.ConsumerGroupSession) error {
 }
 
 func (c *consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
-	for message := range claim.Messages() {
+	for msg := range claim.Messages() {
 		for _, topic := range c.topics {
-			if topic == message.Topic {
-				traceId := GetTraceIdFromHeader(message.Headers)
+			if topic == msg.Topic {
+				traceId := GetTraceIdFromHeader(msg.Headers)
 				if c.tracer != nil && traceId != "" {
 					return xtrace.RunWithTraceHook(c.tracer, oteltrace.SpanKindConsumer, traceId, "saramakafka.ConsumeClaim", func(ctx context.Context) error {
-						return c.handleMessage(session, message)
-					})
+						return c.handleMessageCtx(ctx, session, msg)
+					},
+						attribute.String(mq.TraceMqTopic, msg.Topic),
+						attribute.String(mq.TraceMqKey, string(msg.Value)),
+						attribute.String(mq.TraceMqPayload, string(msg.Value)))
 				} else {
-					return c.handleMessage(session, message)
+					return c.handleMessage(session, msg)
 				}
 			}
 		}
@@ -113,12 +118,16 @@ func (c *consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim saram
 	return nil
 }
 
-func (c *consumer) handleMessage(session sarama.ConsumerGroupSession, message *sarama.ConsumerMessage) error {
-	if err := c.handler(message); err != nil {
-		logx.Errorf("saramakafka.ConsumeClaim.handleMessage on error: %v", err)
+func (c *consumer) handleMessage(session sarama.ConsumerGroupSession, msg *sarama.ConsumerMessage) error {
+	return c.handleMessageCtx(context.Background(), session, msg)
+}
+
+func (c *consumer) handleMessageCtx(ctx context.Context, session sarama.ConsumerGroupSession, msg *sarama.ConsumerMessage) error {
+	if err := c.handler(ctx, msg); err != nil {
+		logx.WithContext(ctx).Errorf("saramakafka.ConsumeClaim.handleMessage on error: %v", err)
 		return err
 	} else {
-		session.MarkMessage(message, "")
+		session.MarkMessage(msg, "")
 	}
 	return nil
 }
