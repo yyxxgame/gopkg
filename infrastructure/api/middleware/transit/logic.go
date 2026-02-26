b5711aef632b2c26cde887c963db29e067a74e33
#Suyghur <suyghurmjp@outlook.com>
2026-02-26 20:05:44 +0800

    fix(infra): fix parse params error on sign_md5 middleware (#145)

--- infrastructure/api/middleware/transit/logic.go
+++ infrastructure/api/middleware/transit/logic.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"context"
 	"encoding/hex"
-	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
@@ -13,7 +12,6 @@ import (
 	"github.com/duke-git/lancet/v2/convertor"
 	"github.com/duke-git/lancet/v2/maputil"
 	"github.com/yyxxgame/gopkg/cryptor/aes"
-	"github.com/yyxxgame/gopkg/exception"
 	"github.com/yyxxgame/gopkg/infrastructure/api/pkg/responder"
 	"github.com/yyxxgame/gopkg/xtrace"
 	gozerotrace "github.com/zeromicro/go-zero/core/trace"
@@ -38,45 +36,41 @@ func New(routerMap map[string]map[string]http.HandlerFunc, encryptionKey string,
 
 func (m *Middleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		exception.Try(func() {
-			headerIsDecrypt := m.responder.GetEncryptionHeader()
-			if r.Header.Get(headerIsDecrypt) == "1" {
-				r.Header.Set(headerIsDecrypt, "1")
-			} else {
-				r.Header.Set(headerIsDecrypt, "0")
-			}
-			bodyBytes, _ := io.ReadAll(r.Body)
-			// Decrypt Body
-			aesBodyString, err := aes.DecryptCbcPkcs7(hex.DecodeString, string(bodyBytes), m.encryptionKey)
-			if err != nil {
-				m.responder.Response(w, r, map[string]any{"code": -3, "msg": "transit error"}, nil)
-				return
-			}
-			jsonParams := make(map[string]any)
-			_ = sonic.UnmarshalString(aesBodyString[m.prevWidths:], &jsonParams)
-			var action string
-			if maputil.HasKey(jsonParams, "action") {
-				action = convertor.ToString(jsonParams["action"])
-			}
-			// Rewrite Request Body
-			jsonParams["ch_conter"] = string(bodyBytes)
-			if jsonParamsBytes, err := json.Marshal(jsonParams); err == nil {
-				r.Header.Set("Content-Type", "application/json")
-				r.Body = io.NopCloser(bytes.NewBuffer(jsonParamsBytes))
-			}
-			// Redirect next
-			actionPath := fmt.Sprintf("/%s", action)
-			nextCall := m.routerMap[r.Method][actionPath]
-			if nextCall != nil && action != "" {
-				_ = xtrace.WithTraceHook(r.Context(), otel.GetTracerProvider().Tracer(gozerotrace.TraceName), oteltrace.SpanKindInternal, action, func(ctx context.Context) error {
-					handler.PrometheusHandler(actionPath, r.Method)(nextCall).ServeHTTP(w, r)
-					return nil
-				})
-			} else {
-				next(w, r)
-			}
-		}).Catch(func(e exception.Exception) {
+		headerIsDecrypt := m.responder.GetEncryptionHeader()
+		if r.Header.Get(headerIsDecrypt) == "1" {
+			r.Header.Set(headerIsDecrypt, "1")
+		} else {
+			r.Header.Set(headerIsDecrypt, "0")
+		}
+		bodyBytes, _ := io.ReadAll(r.Body)
+		// Decrypt Body
+		aesBodyString, err := aes.DecryptCbcPkcs7(hex.DecodeString, string(bodyBytes), m.encryptionKey)
+		if err != nil {
+			m.responder.Response(w, r, map[string]any{"code": -3, "msg": "transit error"}, nil)
+			return
+		}
+		jsonParams := make(map[string]any)
+		_ = sonic.UnmarshalString(aesBodyString[m.prevWidths:], &jsonParams)
+		var action string
+		if maputil.HasKey(jsonParams, "action") {
+			action = convertor.ToString(jsonParams["action"])
+		}
+		// Rewrite Request Body
+		jsonParams["ch_conter"] = string(bodyBytes)
+		if jsonParamsBytes, err := sonic.Marshal(jsonParams); err == nil {
+			r.Header.Set("Content-Type", "application/json")
+			r.Body = io.NopCloser(bytes.NewBuffer(jsonParamsBytes))
+		}
+		// Redirect next
+		actionPath := fmt.Sprintf("/%s", action)
+		nextCall := m.routerMap[r.Method][actionPath]
+		if nextCall != nil && action != "" {
+			_ = xtrace.WithTraceHook(r.Context(), otel.GetTracerProvider().Tracer(gozerotrace.TraceName), oteltrace.SpanKindInternal, action, func(ctx context.Context) error {
+				handler.PrometheusHandler(actionPath, r.Method)(nextCall).ServeHTTP(w, r)
+				return nil
+			})
+		} else {
 			next(w, r)
-		}).Finally(func() {})
+		}
 	}
 }
